010461688df2c8737405a541b8ce127dcd3cfb2f
kento <okaken830@gmail.com>
2026-09-19 08:00:03 +0000

    vite.config: server.host=trueを追加し、npm run devが常にLAN上のスマホ等からアクセス可能になるようにする(--host付け忘れ対策)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // LAN上の他端末(スマホ等)からアクセスできるようにする
+  },
 })
